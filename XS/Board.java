1fb3c12b2a9d262f53850b94eab41d1e678ce729
PedroTFX <pedro.trindade.tfx@gmail.com>
2023-02-19 19:14:03 +0000

    added Documentation to the funcitons already made

--- XS/Board.java
+++ XS/Board.java
@@ -6,8 +6,23 @@ public class Board{
     Piece[] white_Pieces = new Piece[16];
     Piece[] black_Pieces = new Piece[16];
 
+    /**
+     * Initializes the Board, with the pieces int the starting position:
+     * Each player starts with 16 pieces: one king, one queen, two rooks, two knights, two bishops, and eight pawns.
+     * The pieces are set up on the board as follows:
+     * 
+     * a. The two rooks are placed on the corners of the player's own first rank (the row closest to the player).
+     * b. The two knights are placed next to the rooks.
+     * c. The two bishops are placed next to the knights.
+     * d. The queen is placed on the remaining square of her own color. For example, if a player is playing with white pieces, the queen is placed on the white square.
+     * e. The king is placed on the remaining square of his own color, next to the queen.
+     * f. The eight pawns are placed on the second rank (the row in front of the pieces) in front of the other pieces.
+     * 
+     * The players sit on opposite sides of the board, with the white pieces starting on the first rank and the black pieces starting on the eighth rank.
+     * @param board
+     */
     void initialBoard(Piece[] board){
-
+        
     }
 
     public static void main(String[] args) {
--- XS/Piece.java
+++ XS/Piece.java
@@ -1,6 +1,7 @@
 package XS;
 public class Piece{
 
+    int none = 0;
     int pawn = 1;
     int knigh = 3;  
     int bishop= 3;
@@ -11,14 +12,24 @@ public class Piece{
     int pos;
     String type;
 
+    /**
+     * Initialization of the pieces setting the type and its pos
+     * @param type
+     * @param pos
+     */
     public Piece(String type, int pos){
         this.type = type;
         this.pos = pos;
     }
     
     /**
-     * Show available moves of the Piece given by the paramters
-     * Pawns can 
+     * Show available moves of the Piece given by the paramters:
+     * Pawn: Pawns can move one or two squares forward on their first move, and one square forward on all subsequent moves. They capture diagonally, one square forward to the left or right.
+     * Rook: Rooks can move horizontally or vertically, any number of squares, as long as their path is unobstructed. They capture by landing on the square occupied by an opposing piece.
+     * Knight: Knights move in an "L" shape, two squares in a straight line and then one square to the left or right. They are the only pieces that can jump over other pieces. They capture by landing on the square occupied by an opposing piece.
+     * Bishop: Bishops move diagonally, any number of squares, as long as their path is unobstructed. They capture by landing on the square occupied by an opposing piece.
+     * Queen: Queens can move horizontally, vertically, or diagonally, any number of squares, as long as their path is unobstructed. They are the most powerful piece on the board and can move like both rooks and bishops. They capture by landing on the square occupied by an opposing piece.
+     * King: Kings can move one square in any direction. They are the most important piece on the board, and if a player's king is threatened with capture (i.e., "in check"), the player must take steps to remove the threat. The game is won by checkmating the opponent's king, which means putting the king in check and not being able to get out of it on the next move.
      * @param type
      * @param pos
      * @return
